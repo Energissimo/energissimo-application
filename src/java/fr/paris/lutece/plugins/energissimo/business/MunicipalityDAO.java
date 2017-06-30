d3522185f63ad561c774797156f38e0a6226110e
Pierre LEVY <pierrelevy@users.noreply.github.com>
2017-06-30 12:05:50 +0200

    Work in Progress

--- src/java/fr/paris/lutece/plugins/energissimo/business/MunicipalityDAO.java
+++ src/java/fr/paris/lutece/plugins/energissimo/business/MunicipalityDAO.java
@@ -12,14 +12,14 @@ public final class MunicipalityDAO {
 
     // Constants
     private static final String SQL_QUERY_SELECT_BY_NAME = "SELECT name, zipcode FROM energissimo_municipality WHERE name LIKE ? ";
-    	private static final String SQL_QUERY_SELECT_BY_ZIPCODE = "SELECT zipcode, name, code_iris"
+    	private static final String SQL_QUERY_SELECT_BY_ZIPCODE = "SELECT zipcode, name, code_iris, "
                 + " data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, "
                 + " data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, "
                 + " data21, data22, data23, data24, data25, data26, data27, data28, data29, data30, "
-                + " data31, data32, data33 "
+                + " data31, data32, data33, data34, data35 "
                 + " FROM energissimo_iris_data WHERE zipcode = ?";
 
-        private static final int NUM_FIELD = 32;
+        private static final int NUM_FIELD = 35;
 
     List<Municipality> selectByName(String strName) 
     {
@@ -58,6 +58,11 @@ public final class MunicipalityDAO {
                 municipality = new Municipality();
                 municipality.setZipcode( daoUtil.getString( nIndex++ ) );
                 municipality.setName( daoUtil.getString( nIndex++ ));
+            }
+            else
+            {
+                nIndex++;
+                nIndex++;
             }
             
             IrisData irisData = new IrisData();
